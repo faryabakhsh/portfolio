96236ce0031d216e922ceffa5eb2a41c3f44cc75
Farya <faryabakhsh@gmail.com>
2022-07-17 22:19:19 +0500

    first commit

---
+++ src/pages/Education.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Education() {
+  return (
+    <div>
+      <h1>education page</h1>
+    </div>
+  )
+}
+
+export default Education
---
+++ src/pages/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function projects() {
+  return (
+    <div>
+      <h1>projects page</h1>
+    </div>
+  )
+}
+
+export default projects
