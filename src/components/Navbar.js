8c87cca6dd501a776180107fb3ead0f2602bd214
Farya <faryabakhsh@gmail.com>
2022-07-18 19:09:04 +0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
-import React, {useState} from 'react'
-import { Link } from "react-router-dom"
+import React, {useEffect, useState} from 'react'
+import { Link, useLocation } from "react-router-dom"
 import './Navbar.css';
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
 
     const [expandNavbar, setExpandNavbar] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+      setExpandNavbar(false);
+    }, [location]);
 
   return (
     <div className='navbar' id={expandNavbar ? "open" : "close"}>
-        <div className='togglebutton'>
+        <div className='toggleButton'>
             <button onClick= {()=> {
                 setExpandNavbar((prev)=> !prev);
             }}><ReorderIcon /></button>
