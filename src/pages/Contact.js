21cf7eec729d9b3a043717a1c04329b0eb7bf589
Farya Bakhsh <faryabakhsh@gmail.com>
2022-11-20 02:04:49 +0500

    update contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React from "react";
 import "./Contact.css";
-import emailjs from "emailjs-com"
-
+import emailjs from "emailjs-com";
 
 function Contact() {
-    function sendEmail(e) {
-        e.preventDefault();
+  function sendEmail(e) {
+    e.preventDefault();
 
-        emailjs.sendForm("service_k6mw6ur", "template_qn6my0p", e.target, "iDX8dmLxhtHzH6ppM").then(res=>{console.log(res)}).catch(err=>console.log(err));
-    }
+    emailjs
+      .sendForm(
+        "service_k6mw6ur",
+        "template_qn6my0p",
+        e.target,
+        "iDX8dmLxhtHzH6ppM"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
   return (
-    <div className='contact'>
-      <h1>Lets Connect</h1>
-      <form onSubmit={sendEmail}>
-        <label>name</label><br/>
-        <input type="text" name="name"/>
-<br/>
-        <label>Email</label><br/>
-        <input type="email" name="user_email" /><br/>
+    <div className="contact">
+      <div className="container">
+        <h1 className="heading1">Lets Connect</h1>
+        <form onSubmit={sendEmail}>
+          <label className="label">Name</label>
+          <br />
+          <input className="input" type="text" name="name" />
+          <br />
+          <label className="label">Email</label>
+          <br />
+          <input className="input" type="email" name="user_email" />
+          <br />
 
-        <label>Message</label><br/>
-        <textarea name="message" rows="4" /><br/>
-        <input type="submit" value="send" />
-      </form>
+          <label className="label">Message</label>
+          <br />
+          <textarea className="input" name="message" />
+
+          <br />
+          <input className="button" type="submit" value="send" />
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,81 +1,117 @@
-import React from 'react'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 import "./Education.css";
-import SchoolIcon from '@mui/icons-material/School';
+import SchoolIcon from "@mui/icons-material/School";
 
 function Education() {
   return (
-    <div className='education'>
-      <h2>My Education History</h2>
-      <VerticalTimeline >
+    <div className="education">
+      <h2>My Education/Work History</h2>
+      <VerticalTimeline>
         <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        dateClassName='date'
-        date="2019 - 2023" 
-        iconStyle={{ backgroundImage: "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)", color: "#fff" }}
-        icon={<SchoolIcon />}
+          className="vertical-timeline-element--education"
+          dateClassName="date"
+          date="2019 - 2023"
+          iconStyle={{
+            backgroundImage:
+              "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)",
+            color: "#fff",
+          }}
+          icon={<SchoolIcon />}
         >
-            <h3 className="vertical-timeline-element-title">
+          <h3 className="vertical-timeline-element-title">
             National University of Science and Technology - NUST
           </h3>
           <p> BS in Governance and Public Policy</p>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        dateClassName='date'
-        date="2020"
-        iconStyle={{ backgroundImage: "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)", color: "#fff" }}
-        icon={<SchoolIcon />}
+          className="vertical-timeline-element--education"
+          dateClassName="date"
+          date="2020"
+          iconStyle={{
+            backgroundImage:
+              "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)",
+            color: "#fff",
+          }}
+          icon={<SchoolIcon />}
         >
-            <h3 className="vertical-timeline-element-title">
+          <h3 className="vertical-timeline-element-title">
             Exploration Begins
           </h3>
-          <p> I was introduced to tech for the first time, started exploring HTML, CSS and basic Javascript.</p>
+          <p>
+            {" "}
+            I was introduced to tech for the first time, started exploring HTML,
+            CSS and basic Javascript.
+          </p>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        dateClassName='date'
-        date="2020-2021"
-        iconStyle={{ backgroundImage: "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)", color: "#fff" }}
-        icon={<SchoolIcon />}
+          className="vertical-timeline-element--education"
+          dateClassName="date"
+          date="2020-2021"
+          iconStyle={{
+            backgroundImage:
+              "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)",
+            color: "#fff",
+          }}
+          icon={<SchoolIcon />}
         >
-            <h3 className="vertical-timeline-element-title">
+          <h3 className="vertical-timeline-element-title">
             Wild - Career Changing Decision
           </h3>
-          <p> enrolled myself into a minor course of web engineering at university, at the same time teaching myself the fundamentals of front-end technologies through online courses of udemy</p>
+          <p>
+            {" "}
+            enrolled myself into a minor course of web engineering at
+            university, at the same time teaching myself the fundamentals of
+            front-end technologies through online courses of udemy
+          </p>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        dateClassName='date'
-        date="JAN 2022"
-        iconStyle={{ backgroundImage: "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)", color: "#fff" }}
-        icon={<SchoolIcon />}
+          className="vertical-timeline-element--education"
+          dateClassName="date"
+          date="JAN 2022"
+          iconStyle={{
+            backgroundImage:
+              "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)",
+            color: "#fff",
+          }}
+          icon={<SchoolIcon />}
         >
-            <h3 className="vertical-timeline-element-title">
-            REACT.js
-          </h3>
-          <p> Took a professional course from udemy of React - The Complete Guide (incl Hooks, React Router, Redux) - 48 hours long. Made many smaller project along the course and at the end programmed my own personal project in React. </p>
+          <h3 className="vertical-timeline-element-title">REACT.js</h3>
+          <p>
+            {" "}
+            Took a professional course from udemy of React - The Complete Guide
+            (incl Hooks, React Router, Redux) - 48 hours long. Made many smaller
+            project along the course and at the end programmed my own personal
+            project in React.{" "}
+          </p>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        dateClassName='date'
-        date="AUG 2022"
-        iconStyle={{ backgroundImage: "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)", color: "#fff" }}
-        icon={<SchoolIcon />}
+          className="vertical-timeline-element--education"
+          dateClassName="date"
+          date="AUG 2022"
+          iconStyle={{
+            backgroundImage:
+              "linear-gradient(to right, #56317a 0%, #3d99be 31%, #56317a 100%)",
+            color: "#fff",
+          }}
+          icon={<SchoolIcon />}
         >
-            <h3 className="vertical-timeline-element-title">
+          <h3 className="vertical-timeline-element-title">
             Employment at AlfaBolt
           </h3>
           <p> landed my first part time tech job at Alfabolt. </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
-  )
+  );
 }
 
-export default Education
+export default Education;
