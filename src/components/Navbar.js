966d48b078496fd98ab68a4d05ba8ccd84bd47e9
Farya Bakhsh <faryabakhsh@gmail.com>
2022-11-20 19:08:11 +0500

    add cv

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 import ReorderIcon from "@mui/icons-material/Reorder";
+// import faryaCV.pdf from "../../Public/faryaCV"
 
 function Navbar() {
   const [expandNavbar, setExpandNavbar] = useState(false);
@@ -22,15 +23,18 @@ function Navbar() {
           <ReorderIcon />
         </button>
       </div>
-       {/* <h1 className="h1"><a href="">Farya Bakhsh</a></h1>  */}
+      {/* <h1 className="h1"><a href="">Farya Bakhsh</a></h1>  */}
       <div className="links">
-         <Link to="/" className="name">Home</Link> 
+        <Link to="/" className="name">
+          Home
+        </Link>
         <Link to="/about">About</Link>
         <Link to="/education">Education</Link>
         <Link to="/contact">Contact</Link>
-        <Link className="button">Resume</Link>
+        <a className="button" href="faryaCv.pdf" download="faryaCv.pdf">
+          Resume
+        </a>
       </div>
-      
     </div>
   );
 }
